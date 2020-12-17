b34b6b04e171e18ce428981fb4ff520682e080bc
CaroS <carolyn.mary@hotmail.com>
2020-12-17 03:51:36 -0500

    testing jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 //=== INITIATION THE CONNECTION TO MYSQL =========================
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/config.json")[env];
 
